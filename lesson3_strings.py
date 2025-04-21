21fdbc210fb24ea822632b540de1de93dd7b5cd5
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-04-21 20:14:39 +0300

    3 strings

---
+++ lesson3_strings.py
@@ -0,0 +1,36 @@
+some_string = "om\n5     my name is igor HHHHHHHHHH 66666666654   namer  "
+
+# upper = some_string.upper()
+# lower = some_string.lower()
+# title = some_string.title()
+# capitalize = some_string.capitalize()
+# chain = some_string.lower().upper().capitalize()
+#
+# clear_string_spaces = some_string.strip()
+# clear_string_symbols = some_string.strip(" 54naMe")
+# clear_string_symbols_left = some_string.lstrip(" 54naMe")
+# clear_string_symbols_right = some_string.rstrip(" 54naMer")
+#
+# change_inner_text = (some_string.replace("name", "surname", 1)
+#                      .replace("igor", "Igor")
+#                      .replace("namer", "")
+#                      # .replace('6', '5')
+#                      # .replace('5', '6')
+#                      )
+#
+# table = str.maketrans("65", "56", '\n')
+# result_smart_change = some_string.translate(table)
+
+# slices
+
+slices1 = some_string[:]
+slices2 = some_string[0:12]
+slices3 = some_string[12:20]
+slices4 = some_string[::2]
+slices5 = some_string[1:25:2]
+slices6 = some_string[::-2]
+slices7 = some_string[::-1]
+slices8 = some_string[-3:-20:-2]
+
+
+pass
