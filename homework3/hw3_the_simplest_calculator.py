63b8bcf19ebfceb12738d2960dcbe085f99d58fa
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-05-01 16:43:56 +0300

    Code formatted and the division by 0 option is taken

--- homework3/hw3_the_simplest_calculator.py
+++ homework3/hw3_the_simplest_calculator.py
@@ -5,16 +5,18 @@ second_num = float(input("Enter second number: "))
 math_operation = input("Enter an operation, you want to do with these numbers (+, -, *, /): ").strip()
 
 if math_operation == '+':
-    result = first_num+second_num
+    result = first_num + second_num
     print(f"{first_num} + {second_num} = {result}")
 elif math_operation == '-':
-    result = first_num-second_num
+    result = first_num - second_num
     print(f"{first_num} - {second_num} = {result}")
 elif math_operation == '*':
-    result = first_num*second_num
+    result = first_num * second_num
     print(f"{first_num} * {second_num} = {result}")
+elif math_operation == '/' and second_num == 0:
+    print("Zero division error. We cannot divide by 0")
 elif math_operation == '/':
-    result = first_num/second_num
+    result = first_num / second_num
     print(f"{first_num} / {second_num} = {result}")
 else:
     print(f"{math_operation} is not a math symbol for operation!")
