00ba721185e098ce212ca9e6f0eda3e1cb878bbf
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-05-02 13:33:26 +0300

    Lesson 5

---
+++ lesson_5/example_random.py
@@ -0,0 +1,8 @@
+import random
+
+
+print(random.randint(1, 56))
+print(random.random())
+
+choices = [55, 656565, 898989]
+print(random.choice(choices))
---
+++ lesson_5/example_work_with_files.py
@@ -0,0 +1,25 @@
+# my_file = open("game.py")
+# my_file.close()
+
+# only reading
+# with open('game.py', mode='r', encoding='utf-8') as file:
+#     content = file.read()
+#
+# print(content)
+
+
+# writing
+# with open('shop_plan.csv', mode='+w',encoding='utf-8') as file:
+#     for position in range(1, 10+1):
+#         file.write(f"data: {position}\n")
+#
+#     file.write("the end")
+
+
+# appending
+# with open('shop_plan.csv', mode='a',encoding='utf-8') as file:
+#     for position in range(1, 10+1):
+#         file.write(f"data: {position}\n")
+#
+#     file.write("the end\n")
+
