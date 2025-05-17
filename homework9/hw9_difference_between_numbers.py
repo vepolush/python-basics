778e21ee2562c686653bb8a4f69daad8263adf2b
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-05-17 16:33:47 +0300

    Additional space to PEP 8 style was added

--- homework9/hw9_difference_between_numbers.py
+++ homework9/hw9_difference_between_numbers.py
@@ -13,6 +13,7 @@ def difference(*numbers: int | float) -> int | float:
         return numbers[0]
     return 0
 
+
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
