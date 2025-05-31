ae97fe9f4d2ff33eb468062e8d4c48d561a8a1e7
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-05-31 17:33:45 +0300

    Fixed 'super' issue

--- homework13/hw13_students_group.py
+++ homework13/hw13_students_group.py
@@ -20,10 +20,6 @@ class Student(Human):
     """
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
-        self.gender = gender
-        self.age = age
-        self.first_name = first_name
-        self.last_name = last_name
         self.record_book = record_book
 
     def __str__(self) -> str:
