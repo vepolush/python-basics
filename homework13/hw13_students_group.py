4c910e7ae2c8887f4519159e3a3af52b2dbeffd8
Vladyslav Shulepov <vladyslav.shulepov@gmail.com>
2025-06-03 12:48:22 +0300

    Creating personal modules

--- homework13/hw13_students_group.py
+++ homework13/hw13_students_group.py
@@ -1,3 +1,6 @@
+from typing import Self
+
+
 class AddStudentError(Exception):
     """
     Class for 'add student' exceptions description
@@ -36,6 +39,12 @@ class Student(Human):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} has book record №{self.record_book}"
+
+    def __eq__(self, other: Self):
+        return Group.find_student == other
+
+    def __hash__(self):
+        return hash(str(self))
 
     __repr__ = __str__
 
@@ -84,23 +93,23 @@ class Group:
         return f'Number:{self.number}\n{all_students} '
 
 
-st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
-st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
-
-gr = Group('PD1')
-gr.add_student(st1)
-gr.add_student(st2)
-
-print(gr)
-
-assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
-assert gr.find_student('Jobs2') is None, 'Test2'
-assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
-
-gr.delete_student('Taylor')
-print(gr)  # Only one student
-
-gr.delete_student('Taylor')  # No error!
-print(gr)
-
-print("Ok")
+# st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+# st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+#
+# gr = Group('PD1')
+# gr.add_student(st1)
+# gr.add_student(st2)
+#
+# print(gr)
+#
+# assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+# assert gr.find_student('Jobs2') is None, 'Test2'
+# assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+#
+# gr.delete_student('Taylor')
+# print(gr)  # Only one student
+#
+# gr.delete_student('Taylor')  # No error!
+# print(gr)
+#
+# print("Ok")
